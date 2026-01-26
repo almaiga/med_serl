8874a886c3ec62c9ffd24ede5688876a06f941a9
Jos <josmaiga@gmail.com>
2026-01-26 14:14:52 +0000

    change rollout from sglang to hf

--- scripts/self_play/run_training.sh
+++ scripts/self_play/run_training.sh
@@ -102,7 +102,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.entropy_coeff=0.01 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.rollout.name=sglang \
+    actor_rollout_ref.rollout.name=hf \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
