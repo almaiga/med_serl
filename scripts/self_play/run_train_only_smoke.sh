6b236391ac46fdf307c5840cbced782dcdc427c1
Jos <josmaiga@gmail.com>
2026-03-12 18:23:58 +0000

    fix config

--- scripts/self_play/run_train_only_smoke.sh
+++ scripts/self_play/run_train_only_smoke.sh
@@ -284,6 +284,7 @@ python3 -m verl.trainer.main_ppo \
     \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.strategy=fsdp2 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     \
     critic.enable=false \
     \
